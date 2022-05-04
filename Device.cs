0276586dbdff4b9729179b4a4a9f4d233d4d9a71
fsinnes <56710841+fsinnes@users.noreply.github.com>
2022-05-04 15:32:06 -0500

    Removed VID PID for now

--- Device.cs
+++ Device.cs
@@ -7,9 +7,6 @@ namespace bellatrix
         public string PortName { get; set; }
         public SerialPort PortConnection { get; set; }
 
-        public string? VID { get; set; }
-        public string? PID { get; set; }
-
         public string? IMEI { get; set; }
         public string? SerialNo { get; set; }
         public string? ModelNo { get; set; }
